225d11388fed632bd6dea09f46e73047ef3f194f
jacqschweiger <jacqschweiger@gmail.com>
2017-02-11 11:50:00 -0500

    barebones API framework, to be updated once JSON format set up

---
+++ aloha/aloha/AlohaAPIClient.swift
@@ -0,0 +1,60 @@
+//
+//  AlohaAPIClient.swift
+//  aloha
+//
+//  Created by Jacqueline Minneman on 2/11/17.
+//  Copyright © 2017 Michelle Staton. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    //test coordinates = Brooklyn Bridge
+    static let xcoordinate: Double = 40.7
+    static let ycoordinate: Double = 73.9
+}
+
+
+
+
+class AlohaAPIClient {
+    
+    class func getAPIData(with completion: @escaping (String)-> Void) {
+        
+        let urlString: String = "https://requestb.in/1hl4rhx1?x=\(Constants.xcoordinate)&y=\(Constants.ycoordinate)"
+        
+        let url = URL(string: urlString)
+        
+        if let unwrappedURL = url {
+            
+            let session = URLSession.shared
+            
+            let task = session.dataTask(with: unwrappedURL) { (data, response, error) in
+                
+                if let unwrappedData = data {
+                    
+                    do {
+                        
+                        let responseJSON = try "Brooklyn Bridge test"
+                        
+                        //TODO: uncomment line below once JSON set up
+                        
+                        //let responseJSON = try JSONSerialization.jsonObject(with: unwrappedData, options: []) as! [String: AnyObject]
+                        
+                        completion(responseJSON)
+                        
+                        print(responseJSON)
+                        
+                    } catch {
+                        
+                        print(error)
+                    }
+                }
+                
+            }
+            
+            task.resume()
+        }
+    }
+    
+}
