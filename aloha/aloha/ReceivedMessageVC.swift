3544e36d1f4f8608e9b4cdcfd02b19c6b95252f3
MichelleBKlynn <michellestaton@gmail.com>
2017-02-11 12:03:29 -0500

    Added Swift file for VC to receive messages

---
+++ aloha/aloha/ReceivedMessageVC.swift
@@ -0,0 +1,19 @@
+//
+//  ReceivedMessageVC.swift
+//  aloha
+//
+//  Created by Michelle Staton on 2/11/17.
+//  Copyright © 2017 Michelle Staton. All rights reserved.
+//
+
+import Foundation
+
+
+// This will be the screen that displays the sender's message to the user 
+
+
+
+
+// when a message is received it should be sent to this screen 
+
+// eventually the recipient should be able to reply to the sender but that's not MVP
