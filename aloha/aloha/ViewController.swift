968a34f2fed1b1b39506f7aa6ce162f2e63764f3
MichelleBKlynn <michellestaton@gmail.com>
2017-02-11 11:12:32 -0500

    added project

---
+++ aloha/aloha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aloha
+//
+//  Created by Michelle Staton on 2/11/17.
+//  Copyright © 2017 Michelle Staton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
